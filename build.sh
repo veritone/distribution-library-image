e78a17e149bce179e8725ccc3be72a4a5c17ac4c
Quynh Dang <qdang@veritone.com>
2017-11-08 06:39:11 -0800

    one-off

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+if [ $# -eq 0 ] ; then
+	echo "Usage: ./update.sh <docker/distribution tag or branch>"
+	exit
+fi
+
+VERSION=master
+
+# cd to the current directory so the script can be run from anywhere.
+cd `dirname $0`
+
+echo "Fetching and building distribution $VERSION..."
+
+# Create a temporary directory.
+TEMP=`mktemp -d /$TMPDIR/distribution.XXXXXX`
+
+git clone -b $VERSION https://github.com/docker/distribution.git $TEMP
+docker build -t veritone/vt-registry $TEMP
+
+echo "Done."
